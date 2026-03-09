ae25fcd1cd2116330a29f9d92d8ac29940e6abc1
Tanvir <tanbirulislamjj@gmail.com>
2026-03-09 14:15:05 +0600

    added prometheus service for collecting data

--- modules/services/monitoring.nix
+++ modules/services/monitoring.nix
@@ -10,5 +10,25 @@
     };
   };  
 
+  services.prometheus = {
+  enable = true;
+
+  scrapeConfigs = [
+    {
+      job_name = "node";
+      static_configs = [
+        {
+          targets = [ "localhost:9100" ];
+        }
+      ];
+    }
+  ];
+};
+
+  services.prometheus.exporters.node = {
+    enable = true;
+    port = 9100;
+  };
+
 
 }
