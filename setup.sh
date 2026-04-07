9dcb2b69c87d7bca4f89e19596f71c982ae44637
Tanvir <tanbirulislamjj@gmail.com>
2026-04-07 11:48:47 +0600

    made setup.sh generate hw config first

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-echo "Copying config files to /etc/nixos..."
-sudo cp -r * /etc/nixos/
-
 echo "Generating hardware configuration..."
 sudo nixos-generate-config --show-hardware-config > hardware-configuration.nix
+
+echo "Copying config files to /etc/nixos..."
+sudo cp -r * /etc/nixos/
 
 echo "All done!"
 echo "Now run: sudo nixos-rebuild switch"
