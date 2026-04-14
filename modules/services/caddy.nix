c09f7728a0a6da20067cf7470179c2b496204d46
Tanvir <tanbirulislamjj@gmail.com>
2026-04-14 10:32:12 +0600

    fixed virtualHosts redundancy

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -6,42 +6,44 @@
     user = "caddy";
     group = "caddy";
 
-    # Main Dashboard
-    virtualHosts."nixos-server.tail223014.ts.net" = {
-      extraConfig = ''
-        root * /var/www/
-        file_server
-      '';
-    };
+    virtualHosts = {
+      # Main Dashboard
+      "nixos-server.tail223014.ts.net" = {
+        extraConfig = ''
+          root * /var/www/
+          file_server
+        '';
+      };
 
-    # Navidrome
-    virtualHosts."nixos-server.tail223014.ts.net:14533" = {
-      extraConfig = "reverse_proxy 127.0.0.1:4533";
-    };
+      # Navidrome
+      "nixos-server.tail223014.ts.net:14533" = {
+        extraConfig = "reverse_proxy 127.0.0.1:4533";
+      };
 
-    # Slskd
-    virtualHosts."nixos-server.tail223014.ts.net:15030" = {
-      extraConfig = "reverse_proxy 127.0.0.1:5030";
-    };
+      # Slskd
+      "nixos-server.tail223014.ts.net:15030" = {
+        extraConfig = "reverse_proxy 127.0.0.1:5030";
+      };
 
-    # qBittorrent
-    virtualHosts."nixos-server.tail223014.ts.net:18080" = {
-      extraConfig = "reverse_proxy 127.0.0.1:8080" ;
-    };
+      # qBittorrent
+      "nixos-server.tail223014.ts.net:18080" = {
+        extraConfig = "reverse_proxy 127.0.0.1:8080" ;
+      };
 
-    # Focalboard 
-    virtualHosts."nixos-server.tail223014.ts.net:18000" = {
-      extraConfig = "reverse_proxy 127.0.0.1:8000";
-    };
+      # Focalboard
+      "nixos-server.tail223014.ts.net:18000" = {
+        extraConfig = "reverse_proxy 127.0.0.1:8000";
+      };
 
-    # Microbin
-    virtualHosts."nixos-server.tail223014.ts.net:18081" = {
-      extraConfig = "reverse_proxy 127.0.0.1:8081";
-    };
-    
-    # Metadata-remote
-    virtualHosts."nixos-server.tail223014.ts.net:18338" = {
-      extraConfig = "reverse_proxy 127.0.0.1:8338";
+      # Microbin
+      "nixos-server.tail223014.ts.net:18081" = {
+        extraConfig = "reverse_proxy 127.0.0.1:8081";
+      };
+
+      # Metadata-remote
+      "nixos-server.tail223014.ts.net:18338" = {
+        extraConfig = "reverse_proxy 127.0.0.1:8338";
+      };
     };
   };
 }
