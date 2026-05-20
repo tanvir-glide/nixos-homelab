5445684b3ed18b3c4f451391a0af699d7d20f0bb
Tanvir <tanbirulislamjj@gmail.com>
2026-05-20 14:39:02 +0600

    switched to rfc style

--- flake.nix
+++ flake.nix
@@ -64,6 +64,6 @@
       };
 
       # Formatter for consistent code style
-      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
     };
 }
