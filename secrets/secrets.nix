5a117f93117ecc6bc06aacecae256a8dc810c7c0
Tanvir <tanbirulislamjj@gmail.com>
2026-05-20 16:38:43 +0600

    switched to rfc style

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,5 +4,8 @@ let
   user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA4T+hDRRT6cezLQ8VRTYoBLS48HB6fiI0atupZJ6Eko tanvir@nixos";
 in
 {
-  "lastfm.age".publicKeys = [ server user ];
+  "lastfm.age".publicKeys = [
+    server
+    user
+  ];
 }
