b853c6d761a3bc206c221c4f6bdd8ff1cefb09ce
Tanvir101cmd <tanbirulislamjj@gmail.com>
2026-06-12 12:04:32 +0600

    removed: hasedPassword as it is crackable

--- modules/core/users.nix
+++ modules/core/users.nix
@@ -15,9 +15,7 @@
       "networkmanager"
       "docker"
     ];
-
-    hashedPassword = "$6$gzeT9LDJejt49n84$/YkYbEFhO1ZktJJzVEVBkAOL3yhkVCurwcU6B4LAKpJB6wCEkb51HnMVFBuoPo6EXGz08OyBksLzinT.8rX8v1";
-
+    
     shell = pkgs.bash;
 
   };
