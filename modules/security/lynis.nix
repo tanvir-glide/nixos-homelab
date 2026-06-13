853ce9d88f0b4a0116fd0965bafcba15c7c9bcad
Tanvir101cmd <tanbirulislamjj@gmail.com>
2026-06-13 23:56:39 +0600

    sec(kernel): enable structural memory hardening options
    Enables protectKernelImage and forcePageTableIsolation to mitigate kernel-space tampering and side-channel vulnerabilities.
    
    Note: System requires a reboot to initialize these changes.

--- modules/security/lynis.nix
+++ modules/security/lynis.nix
@@ -27,6 +27,10 @@
     "net.ipv6.conf.default.accept_redirects" = 0;
   };
 
+  # Kernel mitigation options
+  security.protectKernelImage = true;
+  security.forcePageTableIsolation = true;
+
   # ----------------------------------------------- Audit daemon ------------------------------------------
 
   security = {
