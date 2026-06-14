385954eb935d0aa67805a4dca26f78552a376705
Tanvir101cmd <tanbirulislamjj@gmail.com>
2026-06-14 16:17:46 +0600

    removed: gotify ssh port leftover

--- modules/security/firewall.nix
+++ modules/security/firewall.nix
@@ -11,7 +11,6 @@ let
     slskd-web = 15030;
     slskd-transfer = 50300; # UDP
     qbittorrent = 18080;
-    gotify = 8085;
     minecraft = 46565;
     tailscale = 41641; # UDP
     microbin = 18081;
